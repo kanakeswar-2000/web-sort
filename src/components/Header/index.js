15fdaf9b506b76107ed143e7d5213bbd9d6a2f9b
kanakeswar-2000 <pkanakeswarareddy@gmail.com>
2025-06-23 08:45:55 +0530

    initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import "./index.css"
+
+const Header =(props)=>{
+    return (
+        <nav className="header-container">
+            <div>
+                <img src="https://res.cloudinary.com/dlmrn1xom/image/upload/v1750586819/2fdcf92ce27321079a9002edc684fad273c7e610_xulmhp.png"/>
+            </div>
+        </nav>
+    )
+}
+export default Header
---
+++ src/components/Home/index.js
@@ -0,0 +1,14 @@
+import "./index.css"
+
+import Header from "../Header"
+
+const Home =()=>{
+    return <div className="home-container"> 
+        <Header />
+        <h1 className="heading">Project</h1>
+        <img src="https://res.cloudinary.com/dlmrn1xom/image/upload/v1750608204/09301d3daf6cd4d731199dc2e4fb42fd1f0cfb3a_jpqbpd.jpg"/>
+        <h1 className="heading">UI/UX Design</h1>
+    </div>
+}
+
+export default Home
